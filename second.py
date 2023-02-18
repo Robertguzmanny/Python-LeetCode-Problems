5b6debade8feea5ce4075fe658f1ad7d3d2a05f0
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2023-02-18 02:46:40 -0500

    manager

--- second.py
+++ second.py
@@ -52,75 +52,75 @@
 #
 # underpaid()
 
-class Manager:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
+# class Manager:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
+#
+# class BST:
+#     def __init__(self):
+#         self.root = None
+#
+#     def is_underpaid(self, threshold):
+#         underpaid_managers = []
+#         def traverse(node):
+#             if node is None:
+#                 return
+#             if node.salary < threshold:
+#                 underpaid_managers.append(node.name)
+#             traverse(node.left)
+#             traverse(node.right)
+#         traverse(self.root)
+#         return underpaid_managers
+#
+# bst = BST()
+#
+# # insert managers into the BST
+# bst.root = Manager("John", 100000)
+# bst.root.left = Manager("Mike", 90000)
+# bst.root.right = Manager("Sarah", 120000)
+#
+# # find underpaid managers
+# threshold = 95000
+# underpaid_managers = bst.is_underpaid(threshold)
+#
+# # print the names of the underpaid managers
+# for manager in underpaid_managers:
+#     print(manager)
+#
+#     def test_underpaid_manager():
+# # Create the binary search tree
+#         bst = BST()
+#
+#     # Insert managers into the BST
+#         bst.root = Manager("John", 100000)
+#         bst.root.left = Manager("Mike", 90000)
+#         bst.root.right = Manager("Sarah", 120000)
+#         bst.root.left.left = Manager("Alex", 85000)
+#         bst.root.left.right = Manager("Jane", 95000)
+#
+#         # Test 1: Test with a threshold that no manager is underpaid
+#         threshold = 100000
+#         assert bst.is_underpaid(threshold) == []
+#
+#         # Test 2: Test with a threshold that all managers are underpaid
+#         threshold = 50000
+#         assert bst.is_underpaid(threshold) == ["John", "Mike", "Sarah", "Alex", "Jane"]
+#
+#         # Test 3: Test with a threshold that some managers are underpaid
+#         threshold = 92000
+#         assert bst.is_underpaid(threshold) == ["Mike", "Alex", "Jane"]
+#
+#         # Test 4: Test with a threshold that only one manager is underpaid
+#         threshold = 90000
+#         assert bst.is_underpaid(threshold) == ["Mike"]
+#
+#         # Test 5: Test with a threshold that no manager exists in the BST
+#         threshold = 200000
+#         assert bst.is_underpaid(threshold) == []
 
-class BST:
-    def __init__(self):
-        self.root = None
 
-    def is_underpaid(self, threshold):
-        underpaid_managers = []
-        def traverse(node):
-            if node is None:
-                return
-            if node.salary < threshold:
-                underpaid_managers.append(node.name)
-            traverse(node.left)
-            traverse(node.right)
-        traverse(self.root)
-        return underpaid_managers
-
-bst = BST()
-
-# insert managers into the BST
-bst.root = Manager("John", 100000)
-bst.root.left = Manager("Mike", 90000)
-bst.root.right = Manager("Sarah", 120000)
-
-# find underpaid managers
-threshold = 95000
-underpaid_managers = bst.is_underpaid(threshold)
-
-# print the names of the underpaid managers
-for manager in underpaid_managers:
-    print(manager)
-
-    def test_underpaid_manager():
-# Create the binary search tree
-        bst = BST()
-
-    # Insert managers into the BST
-        bst.root = Manager("John", 100000)
-        bst.root.left = Manager("Mike", 90000)
-        bst.root.right = Manager("Sarah", 120000)
-        bst.root.left.left = Manager("Alex", 85000)
-        bst.root.left.right = Manager("Jane", 95000)
-
-        # Test 1: Test with a threshold that no manager is underpaid
-        threshold = 100000
-        assert bst.is_underpaid(threshold) == []
-
-        # Test 2: Test with a threshold that all managers are underpaid
-        threshold = 50000
-        assert bst.is_underpaid(threshold) == ["John", "Mike", "Sarah", "Alex", "Jane"]
-
-        # Test 3: Test with a threshold that some managers are underpaid
-        threshold = 92000
-        assert bst.is_underpaid(threshold) == ["Mike", "Alex", "Jane"]
-
-        # Test 4: Test with a threshold that only one manager is underpaid
-        threshold = 90000
-        assert bst.is_underpaid(threshold) == ["Mike"]
-
-        # Test 5: Test with a threshold that no manager exists in the BST
-        threshold = 200000
-        assert bst.is_underpaid(threshold) == []
-
-
-test_underpaid_manager()
+# test_underpaid_manager()
 
 # def manager():
 #
@@ -138,3 +138,20 @@ test_underpaid_manager()
 # print(dict["joe"])
 #
 # manager()
+
+def manager():
+
+    thisdict =	{
+      "brad": 200,
+      "mod": 100,
+      "year": 60
+    }
+    x = thisdict.keys()
+    for i in thisdict:
+        #print(thisdict.get(i))
+        if thisdict.get(i) < 100:
+            print("this person is underpaid")
+        else:
+            return None
+
+print(manager())
