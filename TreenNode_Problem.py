9b3adf027334a70ad357d916549e60fcbbb04d99
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-02-12 05:31:27 -0500

    added comments and examples in treenode

--- TreenNode_Problem.py
+++ TreenNode_Problem.py
@@ -4,6 +4,20 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# q: Given a binary tree, count the number of nodes in each value in python.
+# The output should be a dictionary where the key is the value of the node and the value is the count of the number of nodes with that value.
+# For example, given the following tree:
+#     2
+#    / \
+#   1   3
+#      / \
+#     2   4
+# Return {1: 1, 2: 2, 3: 1, 4: 1}
+
+# A: We can use a depth-first search to traverse the tree and count the number of nodes for each value.
+# We can use a dictionary to store the counts of each value. We can then traverse the tree and increment the count for each value we encounter.
+
+
 
 def counting_nodes(root, counts):
     if root is None:
