e5af2824168535ed4c3091af496b85243cd2ef86
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-04-06 23:35:01 -0400

    character replacement problem

---
+++ longest-repeating-character-replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterRep(self,s, k):
+        longest = 0
+        l = 0
+
+        counts = [0] * 26
+        for r in range(len(s)):
+            counts[ord(s[r]) - 65] += 1
+
+            while (r-l + 1 - max(counts)) > k:
+                counts[ord(s[l]) - 65] -= 1
+                l += 1
+
+            longest = max(longest, r-l+1)
+
+        return longest
+
+test = Solution()
+s = "AABABBA"
+k = 1
+print(test.characterRep(s, k)) # 4
