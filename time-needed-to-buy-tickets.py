a57a268ea3fed47c6c5f1f57d7c99c7d5f88c616
Robertguzmanny <101305148+Robertguzmanny@users.noreply.github.com>
2024-04-09 00:21:43 -0400

    leetcode 2073

---
+++ time-needed-to-buy-tickets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets, k):
+        res = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                res += min(tickets[i], tickets[k])
+
+            else:
+               res +=  min(tickets[i], tickets[k] - 1)
+
+
+        return res
+
+test = Solution()
+tickets = [5,1,1,1]
+k = 0
+print(test.timeRequiredToBuy(tickets, k))
